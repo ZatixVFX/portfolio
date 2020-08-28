b546e0e404fff4e1f9343a31e9a105b6465e6231
Junaid Salie <ZatixVFX@users.noreply.github.com>
2020-08-28 04:59:28 -0700

    initial commit

--- src/components/pages/testimonials/TestimonialSection.js
+++ src/components/pages/testimonials/TestimonialSection.js
@@ -12,6 +12,7 @@ const TestimonialSection = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    arrows: true,
   };
   return (
     <section className="hero is-medium is-medium-style" id="Testimonials">
@@ -20,37 +21,35 @@ const TestimonialSection = () => {
         <h2 className="subtitle">This is what my peers have to say about me</h2>
       </div>
       <div className="hero-body">
-        <Slider {...settings}>
+        <Slider {...settings} autoplay={5000} className="columns">
           {Testimonials.map((t, id) => (
-            <div className="columns">
+            <div
+              className="column is-12"
+              key={id}
+              style={{ marginLeft: "1rem" }}
+            >
               <div
-                className="column is-12"
-                key={id}
-                style={{ marginLeft: "1rem" }}
+                className="card is-flex"
+                style={{ padding: "4.25rem !important" }}
               >
-                <div
-                  className="card is-flex"
-                  style={{ padding: "4.25rem !important" }}
-                >
-                  <div className="card-image ml-4">
-                    <figure
-                      className="image"
-                      style={{ height: "197px", width: "332px" }}
-                    >
-                      <img
-                        src={t.img}
-                        alt={t.name}
-                        style={{ width: "100%", maxHeight: "100%" }}
-                      />
-                    </figure>
-                  </div>
-                  <div className="content" style={{ marginLeft: "3rem" }}>
-                    <p style={{ fontSize: "25px" }}>
-                      <b>&#8220;</b>
-                      {t.qoute}
-                      <b>&#8221;</b> - <b>{t.name}</b>
-                    </p>
-                  </div>
+                <div className="card-image ml-4">
+                  <figure
+                    className="image"
+                    style={{ height: "197px", width: "332px" }}
+                  >
+                    <img
+                      src={t.img}
+                      alt={t.name}
+                      style={{ width: "100%", maxHeight: "100%" }}
+                    />
+                  </figure>
+                </div>
+                <div className="content" style={{ marginLeft: "3rem" }}>
+                  <p style={{ fontSize: "25px" }}>
+                    <b>&#8220;</b>
+                    {t.qoute}
+                    <b>&#8221;</b> - <b>{t.name}</b>
+                  </p>
                 </div>
               </div>
             </div>
